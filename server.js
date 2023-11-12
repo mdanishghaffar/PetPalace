8eaaf7a4dbf377f573314f3eaa979b702d6af7a3
Qaim Raza <112265197+syedQaimr@users.noreply.github.com>
2023-11-13 02:36:13 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import dotenv from 'dotenv';
 import express from "express";
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+<<<<<<< HEAD
 import authRoute from './routes/authRoute.js'
 import productRoutes from "./routes/productRoutes.js"
+=======
+import authRoute from './Routes/authrouter.js'
+import productrouter from './Routes/productrouter.js'
+import cors from "cors";
+// import productRoutes from "./routes/productRoutes.js"
+>>>>>>> 24081ed (Initial files)
 
 
 //config file
@@ -17,6 +24,7 @@ connectDB();
 const app=express()
 
 //routes
+<<<<<<< HEAD
 app.use('/api/vi/auth',authRoute)
 app.use('/api/vi/product',productRoutes)
 
@@ -25,6 +33,20 @@ app.use(morgan('dev'))
 
 // rest api
 
+=======
+app.use('/api/v1/auth',authRoute)
+app.use('/api/v1/product',productrouter)
+
+
+
+//Middleware
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+// rest api
+>>>>>>> 24081ed (Initial files)
 app.get('/',(req,res)=>{
     res.send("<h1>welcome to Ecommerce Pet Store</h1>")
 })
